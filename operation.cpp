c694ab68d67a6a1add572619140584b735e1738a
maxcorralesr <maxcorralesr@gmail.com>
2025-11-26 21:33:51 -0700

    Archivos base

---
+++ operation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by maxco on 11/26/2025.
+//
+
+#include "operation.h"
---
+++ operation.h
@@ -0,0 +1,13 @@
+//
+// Created by maxco on 11/26/2025.
+//
+
+#ifndef UNTITLED5_OPERATION_H
+#define UNTITLED5_OPERATION_H
+
+
+class operation {
+};
+
+
+#endif //UNTITLED5_OPERATION_H
