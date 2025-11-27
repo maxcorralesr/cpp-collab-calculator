ead8f58b18b38d2dcc998c13f3a64270f1edbe46
HLT.06 <a01256493@tec.mx>
2025-11-26 23:07:53 -0700

    correcion

--- operation.h
+++ operation.h
@@ -2,14 +2,12 @@
 // Created by maxco on 11/26/2025.
 //
 
-#ifndef UNTITLED5_OPERATION_H
-#define UNTITLED5_OPERATION_H
+#ifndef OPERATION_H
+#define OPERATION_H
 
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
 
-class operation {
-    double add(double a, double b);
-    double subtract(double a, double b);
-};
-
-
-#endif //UNTITLED5_OPERATION_H
+#endif 
